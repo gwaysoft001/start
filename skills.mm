5502c6df33db9ab4e2914ddddc6e7cd71248f623
gwaysoft001 <gwaysoft@outlook.com>
2019-03-05 17:51:24 +0800

    add skills.mm

---
+++ skills.mm
@@ -0,0 +1,435 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1551770828392" ID="ID_691206819" MODIFIED="1551770997686" TEXT="skills">
+<node CREATED="1551770960822" ID="ID_1189609215" MODIFIED="1551770980745" POSITION="right" TEXT="Operating System">
+<node CREATED="1551771390235" ID="ID_1954920768" MODIFIED="1551771394807" TEXT="linux">
+<node CREATED="1551771403043" ID="ID_762738337" MODIFIED="1551775820973" TEXT="CentOS">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      website: https://www.centos.org/
+    </p>
+    <p>
+      current version: CentOS-7-x86_64-DVD-1810.iso
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1551771854883" ID="ID_1892302707" MODIFIED="1551771864166" TEXT="command">
+<node CREATED="1551771830995" ID="ID_1190437359" MODIFIED="1551772221556" TEXT="network">
+<node CREATED="1551771868059" ID="ID_825215649" MODIFIED="1551776228694" TEXT="ifconfig">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Q
+    </p>
+    <p>
+      discard (CentOS-7)
+    </p>
+    <p>
+      A
+    </p>
+    <p>
+      yum search ifconfig
+    </p>
+    <p>
+      yum install net-tools.x86_64
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1551772241548" ID="ID_250064596" MODIFIED="1551772289972" TEXT="practice">
+<node CREATED="1551772289973" ID="ID_1420344602" MODIFIED="1551772298144" TEXT="ifconfig"/>
+</node>
+<node CREATED="1551773125052" ID="ID_1513391005" MODIFIED="1551776304296" TEXT="reslove">
+<node CREATED="1551773157259" ID="ID_457293659" MODIFIED="1551776155401" TEXT="ifconfig: command not found">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Q
+    </p>
+    <p>
+      # ifconfig
+    </p>
+    <p>
+      -bash: ifconfig: command not found
+    </p>
+    <p>
+      A
+    </p>
+    <p>
+      1
+    </p>
+    <p>
+      echo $PATH
+    </p>
+    <p>
+      add /usr/sbin
+    </p>
+    <p>
+      vi /etc/profile
+    </p>
+    <p>
+      export PATH=$PATH:/sbin
+    </p>
+    <p>
+      2
+    </p>
+    <p>
+      yum
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#160;
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551772221557" ID="ID_1256592906" MODIFIED="1551772236516" TEXT="ip">
+<node CREATED="1551772318795" ID="ID_629367899" MODIFIED="1551776254713" TEXT="practice">
+<node CREATED="1551772322429" ID="ID_968197514" MODIFIED="1551776252075" TEXT="ip a">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ip addr
+    </p>
+    <p>
+      ip addr show
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551772407675" ID="ID_710385014" MODIFIED="1551772411748" TEXT="ping">
+<node CREATED="1551772411749" ID="ID_1147294084" MODIFIED="1551772426532" TEXT="practice">
+<node CREATED="1551772426533" ID="ID_983941100" MODIFIED="1551772445264" TEXT="ping www.aliyun.com"/>
+</node>
+</node>
+<node CREATED="1551772982155" ID="ID_737214527" MODIFIED="1551773015876" TEXT="service">
+<node CREATED="1551773028331" ID="ID_213520407" MODIFIED="1551773047640" TEXT="practice">
+<node CREATED="1551773015877" ID="ID_571959361" MODIFIED="1551773024555" TEXT="service network restart"/>
+</node>
+</node>
+</node>
+<node CREATED="1551776347099" ID="ID_1963052222" MODIFIED="1551776357504" TEXT="install">
+<node CREATED="1551773629156" ID="ID_371840669" MODIFIED="1551773632736" TEXT="yum">
+<node CREATED="1551776365091" ID="ID_757082662" MODIFIED="1551776372184" TEXT="resolve">
+<node CREATED="1551773686915" ID="ID_1620302082" MODIFIED="1551776581646" TEXT="revise CentOS-Base.repo">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A
+    </p>
+    <p>
+      cd /etc/yum.repos.d
+    </p>
+    <p>
+      backup: mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+    </p>
+    <p>
+      download repo's file: wget http://mirrors.163.com/.help/CentOS6-Base-163.repo
+    </p>
+    <p>
+      set default file: mv CentOS6-Base-163.repo CentOS-Base.repo
+    </p>
+    <p>
+      yum clean all
+    </p>
+    <p>
+      yum makecache
+    </p>
+    <p>
+      test: yum search ifconfig
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1551779234683" ID="ID_151450836" MODIFIED="1551779239639" TEXT="common">
+<node CREATED="1551779241011" ID="ID_1279201537" MODIFIED="1551779324794" TEXT="mount">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Q
+    </p>
+    <p>
+      mount cdrom
+    </p>
+    <p>
+      A
+    </p>
+    <p>
+      mkdir /media/cdrom
+    </p>
+    <p>
+      mount /dev/cdrom /media/cdrom
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551773995387" ID="ID_721970981" MODIFIED="1551776902368" TEXT="tools">
+<node CREATED="1551774000531" ID="ID_1832416529" MODIFIED="1551776917656" TEXT="ssh">
+<node CREATED="1551776919796" ID="ID_299376131" MODIFIED="1551778539392" TEXT="PuTTy">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      website: https://www.chiark.greenend.org.uk/~sgtatham/putty/
+    </p>
+    <p>
+      PSCP (PuTTY Secure Copy client)
+    </p>
+    <p>
+      PSFTP (PuTTY SFTP client)
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1551777020955" ID="ID_562102588" MODIFIED="1551778713437" TEXT="install">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      download: https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1551778700043" ID="ID_391807414" MODIFIED="1551778707004" TEXT="PuTTY"/>
+<node CREATED="1551777035891" ID="ID_880366157" MODIFIED="1551777611534" TEXT="pscp">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      manual: https://www.cnblogs.com/shijiaqi1066/p/3843207.html
+    </p>
+    <p>
+      cd PuTTY directory
+    </p>
+    <p>
+      pscp source user@host:target
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1551778264292" ID="ID_130182963" MODIFIED="1551778688575" TEXT="psftp">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cd PuTTY directory
+    </p>
+    <p>
+      psftp
+    </p>
+    <p>
+      psftp&gt; open root@192.168.2.2 22
+    </p>
+    <p>
+      password:
+    </p>
+    <p>
+      psftp&gt; pwd
+    </p>
+    <p>
+      psftp&gt; put source
+    </p>
+    <p>
+      command: https://blog.csdn.net/lawliet233/article/details/79430975
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1551778882371" ID="ID_1405253464" MODIFIED="1551778888944" TEXT="SecureCRT"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1551771009292" ID="ID_664772960" MODIFIED="1551775701813" POSITION="left" TEXT="VMWare Workstation Pro">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      website: https://www.vmware.com/products/workstation-pro.html
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1551771276733" ID="ID_1728370365" MODIFIED="1551775637992" TEXT="install">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      download: https://www.vmware.com/products/workstation-pro.html
+    </p>
+    <p>
+      current version: VMware-workstation-full-15.0.2-10952284.exe
+    </p>
+    <p>
+      current version keys: ZC10K-8EF57-084QZ-VXYXE-ZF2XF
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1551771341644" ID="ID_890149336" MODIFIED="1551771370392" TEXT="CentOS7">
+<node CREATED="1551771640139" ID="ID_1708292391" MODIFIED="1551775460821" TEXT="network setting">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # ping www.163.com
+    </p>
+    <p>
+      ping: www.163.com: Name or service not known
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      https://www.linuxidc.com/Linux/2017-12/149910.htm
+    </p>
+    <p>
+      https://www.cnblogs.com/mythdoraemon/p/10462179.html
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551774056509" ID="ID_1154409754" MODIFIED="1551776644213" POSITION="right" TEXT="Java SE">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      website: https://www.oracle.com/technetwork/java/javase/overview/index.html
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1551774176363" ID="ID_1696620606" MODIFIED="1551777014450" TEXT="Java SE 8">
+<node CREATED="1551774224683" ID="ID_590944319" MODIFIED="1551777018132" TEXT="install by exe">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      download: https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+    </p>
+    <p>
+      current version: jdk-8u201-windows-x64.exe
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551775055923" ID="ID_1618162390" MODIFIED="1551775061544" POSITION="left" TEXT="tools">
+<node CREATED="1551775063659" ID="ID_1384244475" MODIFIED="1551775593654" TEXT="freemind">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      website: freemind.sourceforge.net/wiki/index.php/Main_Page
+    </p>
+    <p>
+      download: freemind.sourceforge.net/wiki/index.php/Download
+    </p>
+    <p>
+      current version: FreeMind-Windows-Installer-1.0.1-max.exe
+    </p>
+    <p>
+      system requirement: Java Runtime Environment 1.6 or later.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
