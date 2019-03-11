c8f1373956f519f8561bdfa54fd4039573281bdf
gwaysoft001 <gwaysoft@outlook.com>
2019-03-11 17:53:05 +0800

    1903

--- skills.mm
+++ skills.mm
@@ -134,20 +134,20 @@
 </node>
 <node CREATED="1551776347099" ID="ID_1963052222" MODIFIED="1551776357504" TEXT="install">
 <node CREATED="1551773629156" ID="ID_371840669" MODIFIED="1552271539776" TEXT="yum">
-<node CREATED="1551947218805" ID="ID_511538169" MODIFIED="1552271525017" TEXT="config">
+<node CREATED="1551947218805" ID="ID_511538169" MODIFIED="1552275453195" TEXT="config">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      A
+      Q
     </p>
     <p>
       revise CentOS-Base.repo
     </p>
     <p>
-      Q
+      A
     </p>
     <p>
       cd /etc/yum.repos.d
@@ -169,9 +169,6 @@
     </p>
     <p>
       test: yum search ifconfig
-    </p>
-    <p>
-      
     </p>
     <p>
       
@@ -202,8 +199,7 @@
       yum install -- Install a package or packages on your system
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -239,8 +235,7 @@
       mount /dev/sda1 /media/usb
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1552273870924" ID="ID_931416290" MODIFIED="1552274064478" TEXT="shutdown">
 <richcontent TYPE="NOTE"><html>
@@ -258,8 +253,147 @@
       https://www.cnblogs.com/ningvsban/p/3663337.html
     </p>
   </body>
+</html></richcontent>
+</node>
+<node CREATED="1552276306091" ID="ID_1251203476" MODIFIED="1552276395075" TEXT="source">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      source filename | . filename
+    </p>
+  </body>
 </html>
 </richcontent>
+</node>
+</node>
+<node CREATED="1552275206507" ID="ID_575303073" MODIFIED="1552275209843" TEXT="tools">
+<node CREATED="1552275209844" ID="ID_744986189" MODIFIED="1552290757274" TEXT="vi &amp; vim">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Q<br />alias vi='vim'
+    </p>
+    <p>
+      A
+    </p>
+    <p>
+      vim ~/etc/.bachrc
+    </p>
+    <p>
+      alias vi='vim'
+    </p>
+    <p>
+      source .bachrc | . .bachrc
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Q
+    </p>
+    <p>
+      set configuration
+    </p>
+    <p>
+      A
+    </p>
+    <p>
+      https://blog.csdn.net/u013920085/article/details/46953293
+    </p>
+    <p>
+      temporary
+    </p>
+    <p>
+      : syntax on | off
+    </p>
+    <p>
+      : set number | nonumber
+    </p>
+    <p>
+      : set autoindent
+    </p>
+    <p>
+      : set cindent
+    </p>
+    <p>
+      permanent
+    </p>
+    <p>
+      vi /etc/vimrc | ~/.vimrc
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552276484556" ID="ID_884884653" MODIFIED="1552289381794" TEXT="command mode">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ESC -- command mode
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#160;http://www.runoob.com/linux/linux-vim.html
+    </p>
+    <p>
+      h,j,k,l -- control cursor
+    </p>
+    <p>
+      0,$ -- first, last in a line
+    </p>
+    <p>
+      Ctrl + f,b,d,u -- page
+    </p>
+    <p>
+      gg -- first line
+    </p>
+    <p>
+      Shift + g --last line
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ndd -- cut n line
+    </p>
+    <p>
+      nyy -- copy n line
+    </p>
+    <p>
+      p -- paste
+    </p>
+    <p>
+      nx -- delete n words
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1552276495458" ID="ID_1230984381" MODIFIED="1552289377130" TEXT="insert mode">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      :wq -- quit and save
+    </p>
+    <p>
+      :q! -- quit and not save
+    </p>
+  </body>
+</html></richcontent>
+</node>
 </node>
 </node>
 </node>
@@ -362,7 +496,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1551948235253" ID="ID_428878726" MODIFIED="1551948240709" TEXT="service">
+<node CREATED="1551948235253" FOLDED="true" ID="ID_428878726" MODIFIED="1552275180420" TEXT="service">
 <node CREATED="1551945820801" ID="ID_408963751" MODIFIED="1551946080276" TEXT="bind">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -399,6 +533,41 @@
 <node CREATED="1551947324457" ID="ID_481801861" MODIFIED="1551947341025" TEXT="ifconfig"/>
 <node CREATED="1551947342772" ID="ID_686771029" MODIFIED="1551947345166" TEXT="netstat"/>
 </node>
+</node>
+</node>
+<node CREATED="1552291370894" ID="ID_1976790810" MODIFIED="1552292217998" TEXT="shell">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      echo -e &quot;\e[1;35m David Way \e[1;30m&quot; $(tput sgr0)
+    </p>
+    <p>
+      echo -e &quot;\e[1;35m David Way \e[1;0m&quot;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552293625571" ID="ID_364845061" MODIFIED="1552293689841" TEXT="bracket">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://www.cnblogs.com/hanggegege/p/5928870.html
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
 </node>
