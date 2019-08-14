ac5d6a0d6fecd90e314a27008b84daa49578fc9c
gwaysoft001 <gwaysoft@outlook.com>
2019-08-14 11:21:26 +0800

    start

--- international_trade.mm
+++ international_trade.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1565749538783" ID="ID_939933642" MODIFIED="1565749590372" TEXT="international trade">
 <node CREATED="1565749716302" ID="ID_1889189285" MODIFIED="1565749717835" POSITION="right" TEXT="book">
-<node CREATED="1565749721422" ID="ID_66481586" MODIFIED="1565749721422" TEXT=""/>
+<node CREATED="1565749721422" ID="ID_66481586" MODIFIED="1565749740684" TEXT="&#x5916;&#x8d38;&#x4e03;&#x65e5;&#x901a;"/>
 </node>
 </node>
 </map>
